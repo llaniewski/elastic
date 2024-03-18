714355a5e1126ef785b528115b76fc080b4223b2
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2024-03-19 00:50:07 +1000

    Adding paraview filter

---
+++ filter.py
@@ -0,0 +1,11 @@
+input0 = inputs[0]
+if input0.IsA("vtkImageData"):
+    ext = input0.GetExtent()
+    U = input0.CellData["U"]
+    shp = [ext[1]-ext[0],ext[3]-ext[2],ext[5]-ext[4],3]
+    nU = U.reshape(shp)
+    nU = concatenate([nU,nU[[0],:,:,:]],axis=0)
+    nU = concatenate([nU,nU[:,[0],:,:]],axis=1)
+    nU = concatenate([nU,nU[:,:,[0],:]],axis=2)
+    nU = nU.reshape([-1,3])
+    output.PointData.append(nU, "nU")
